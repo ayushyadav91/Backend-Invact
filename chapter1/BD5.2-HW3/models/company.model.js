ed6727280870897fa543f54f40a36cc477bb8125
theayush434 <theayush434@gmail.com>
2024-10-03 07:07:00 +0530

    db changes done

--- chapter1/BD5.2-HW3/models/company.model.js
+++ chapter1/BD5.2-HW3/models/company.model.js
@@ -1,1 +1,33 @@
-let {DataType, sequelize} = 
+const { sequelize, DataTypes } = require('../lib/index');
+
+
+const Company = sequelize.define('Company', {
+     name: {
+       type: DataTypes.STRING,
+       allowNull: false,
+     },
+     industry: {
+       type: DataTypes.STRING,
+       allowNull: false,
+     },
+     foundedYear: {
+       type: DataTypes.INTEGER,
+       allowNull: false,
+      
+     },
+     headquarters: {
+       type: DataTypes.STRING,
+       allowNull: false,
+     },
+     revenue: {
+       type: DataTypes.FLOAT, // use FLOAT for large numbers like revenue
+       allowNull: false,
+       validate: {
+         min: 0, // revenue can't be negative
+       },
+     },
+   }, {
+     timestamps: true, // to include createdAt and updatedAt fields
+   });
+   
+   module.exports = {Company};
